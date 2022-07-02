245ea17d1abb5e91c735838ae2e9ee396f25e42b
davideva93 <Davideinf@protonmail.com>
2022-07-02 06:03:17 +0200

    Logique localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -17,11 +17,25 @@ return res.json()
                         {let numberOfPresetColor=document.getElementById("colors").length-1
                             if(numberOfPresetColor<numberOfColor) {let  clone = colors.lastElementChild.cloneNode(true); 
                                 {colors.appendChild(clone);}} //si les couleurs préexistantes (2) sont moins que les couleurs du sofa choisi, on clone une couleur préexistante pour pouvoir ensuite effectuer le remplacement
-                            document.getElementById("colors")[a+1].innerHTML=data[i].colors[a]; } //remplace les couleur standard avec les vrais
+                            document.getElementById("colors")[a+1].innerHTML=data[i].colors[a];
+                            document.getElementById("colors")[a+1].value=data[i].colors[a]} //remplace les couleur standard avec les vrais
 
                 
                 } ;}})
 
+addToCart.addEventListener("click",ajouterAuPanier)
 
-                let  clone = parent.lastElementChild.cloneNode(true); 
-                if(i<data.length-1) {parent.appendChild(clone);}
+function ajouterAuPanier() {let idCanape = window.location.href.split("=")[1] //pour trouver l'id du produit
+let colorCanape=document.getElementById("colors").value
+let quantityCanape=document.getElementById("quantity").value
+    if(document.getElementById("quantity").value==0) {alert("Il faut choisir une quantité")} //controle quantité
+    else if (colorCanape==""){alert("il faut choisir une couleur")}//controle couleur
+    //quantité et couleur choisis:
+    else if( window.localStorage.getItem(idCanape+colorCanape)== null)   //si le produit n'existe pas dans le localStorage il faut le créer
+        {window.localStorage.setItem(idCanape+colorCanape,colorCanape+","+quantityCanape )}            
+    else if(window.localStorage.getItem(idCanape+colorCanape).split(",")[0]==colorCanape)  //si le produit existe déjà et la couleur est la pareille, il faut convertir la quantité presente et la quantité à ajouter en number, faire l'addition et mettre à jour
+        {quantityCanape=parseInt(quantityCanape)+parseInt(window.localStorage.getItem(idCanape+colorCanape).split(",")[1]); window.localStorage.setItem(idCanape+colorCanape,colorCanape+","+quantityCanape )}
+    else{console.log("Erreur ligne 38")}
+
+
+}
