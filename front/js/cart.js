f1769e24a89da7f089b51d99a638d1477c89c751
davideva93 <Davideinf@protonmail.com>
2022-07-03 10:10:42 +0200

    progrès cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,34 +8,63 @@ return res.json()
 
 
 let numberProduitsDifferentsPanier = Object.keys(localStorage).length
-cloneArticlePanier()
+cloneArticlePanier() //pour créer x articles dans le panier; x= quantité art. differents
 function cloneArticlePanier() { for(let i=0; i<numberProduitsDifferentsPanier-1; i++) 
     {let  clone = cart__items.lastElementChild.cloneNode(true); 
         cart__items.appendChild(clone);}}
-var k=0;
-let idPanier;
-
-function getIdPanier() 
-        {let nProduitsEnVente=data.length; numberProduitsDifferentsPanier = Object.keys(localStorage).length; 
-            if(k<numberProduitsDifferentsPanier) // boucle primaire de k fois; k= n produits differents dans le panier
-                {k=k+1; for(let i=0; i<nProduitsEnVente; i++) //boucle secondaire; pour chaque k, boucle i fois; i=n produits en vente
-                    {Object.keys(localStorage)[k-1].includes(data[i]._id)? // on compare l'id du 1er produit dans le panier avec les id des produits en vente
-                         idPanier=idPanier+","+(data[i]._id)+":"+i:false} ;getIdPanier()} else return} //+i servira ensuite pour recuperer la position de l'id
 
 
+let idArray =[] ; //array contenant tous les id des produits en vente
+ function idArrayf() {for(let i=0; i<data.length; i++) {idArray.push(data[i]._id)}} 
 
-function affichageDetails()
-            {for(let i=1; i<idPanier.split(":").length; i++)
-                {let a=idPanier.split(":")[i][0]; console.log(i); document.querySelectorAll(".cartAndFormContainer img")[i].currentSrc=data[a].imageUrl }
-            } //a corriger
+ 
 
-   getIdPanier()
-   affichageDetails()
-     
+let panierIdArray=[]; //array contentant tous les id des produits differents achetés
+ function panierIdArrayf() {for(let i=0; i<Object.keys(localStorage).length; i++)  
+    {panierIdArray.push(Object.keys(localStorage)[i].split(",")[0])}}
+                      
+
+setTimeout( idArrayf, 500)
+panierIdArrayf()
+setTimeout(idPositionf,1000)
+setTimeout(detailsPanier,1500)
+let idPosition=[] //array contenant les positions des articles achetés dans le catalogue de vente
+function idPositionf(){
+    for (const value of panierIdArray) {
+        for(let i=0; i<idArray.length; i++) {if(value==idArray[i]) idPosition.push(i)}
+}}
+
+function detailsPanier () {let a=0
+
+    for(let i=0; i<panierIdArray.length; i++) {
+            document.querySelectorAll(".cart__item__content__description h2")[i].innerHTML=data[idPosition[i]].name;//vise le seul h2 de [i] (le nom du produit) et le met à jour
+            document.querySelectorAll("input.itemQuantity")[i].value=window.localStorage.getItem(Object.keys(localStorage)[i]).split(",")[1] //vise le seul input.quantity de [i]  et le met à jour
+            document.querySelectorAll(".cart__item__img img")[i].src=data[idPosition[i]].imageUrl;
+            document.querySelectorAll(".deleteItem")[i].addEventListener("click", function() {document.querySelectorAll("article.cart__item ")[i].remove()}) //button supprimer
+            if(i==0) { //si [i] est 0 on vise les deux <p> presents dans l'article (couleur et prix). Dans ce cas, <p>[0] et <p>[1]
+            document.querySelectorAll(".cart__item__content__description p")[i].innerHTML=Object.keys(localStorage)[i].split(",")[1];
+            document.querySelectorAll(".cart__item__content__description p")[i+1].innerHTML=data[idPosition[i]].price;}
+            else if(i>0 && i%2==1){ //si [i] est impair on vise les deux <p> de l'article en pos  impair. Pour i=1, on vise <p>[2]et <p>[3], pour i=3, <p>[6]et[7] etc
+            document.querySelectorAll(".cart__item__content__description p")[i+1+a].innerHTML=Object.keys(localStorage)[i].split(",")[1];//135
+            document.querySelectorAll(".cart__item__content__description p")[i+2+a].innerHTML=data[idPosition[i]].price;
+            a=a+1;} //à chaque boucle à partir de i>0, a augmente pour viser les <p> correctement
+
+            else if(i>0 && i%2==0){ //si i est pair [0] et>0 on vise les deux <p> de l'article en pos pair. Pour i=2, on vise <p>[4]et <p>[5], pour i=4, <p>[8]et[9] etc
+            document.querySelectorAll(".cart__item__content__description p")[i+1+a].innerHTML=Object.keys(localStorage)[i].split(",")[1];
+            document.querySelectorAll(".cart__item__content__description p")[i+2+a].innerHTML=data[idPosition[i]].price;
+            a=a+1}
+            else {console.log("entrato nell'else")}
+            }}
+
+//total=total+parseInt(document.querySelectorAll(".cart__item__content__description p")[i+2+a].textContent); console.log(total)
 
 
+//var k=0;
+//let idPanier;
 
-
-
-
-   // {parseInt(window.localStorage.getItem(Object.keys(localStorage)[i]).split(",")[1])
+//function getIdPanier() 
+  //      {let nProduitsEnVente=data.length; numberProduitsDifferentsPanier = Object.keys(localStorage).length; 
+    //        if(k<numberProduitsDifferentsPanier) // boucle primaire de k fois; k= n produits differents dans le panier
+      //          {k=k+1; for(let i=0; i<nProduitsEnVente; i++) //boucle secondaire; pour chaque k, boucle i fois; i=n produits en vente
+        //            {Object.keys(localStorage)[k-1].includes(data[i]._id)? // on compare l'id du 1er produit dans le panier avec les id des produits en vente
+          //               idPanier=idPanier+","+(data[i]._id)+":"+i:false} ;getIdPanier()} else return} //+i servira ensuite pour recuperer la position de l'id
